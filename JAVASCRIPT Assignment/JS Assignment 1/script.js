5e039cba9d5ecbc895f4abb71f9fecf5ea664d09
Muhammad Siddique <siddiqueghauri123@gmail.com>
2025-05-25 14:50:24 +0500

    Add files via upload

---
+++ JAVASCRIPT Assignment/JS Assignment 1/script.js
@@ -0,0 +1,115 @@
+
+    // Q1: Greet your website visitor
+    // alert("Welcome to Our Website");
+
+    // Q2: Display message
+    // alert("Error! Please Enter a valid password");
+
+    // Q3: Display message with line break
+    // alert("Welcome to JS Land...\nHappy Coding!");
+
+    // Q4: Display 3 alerts in sequence
+    // alert("Hello!");
+    // alert("Welcome to JS Practice");
+    // alert("Let's learn JavaScript.");
+
+    // Q7: Declare variable username
+    // var username;
+
+    // Q8: Declare variable myName and assign full name
+    // var myName = "Muhammad Siddique";
+
+    // Q9: Declare variable message and assign "Hello World"
+    // var message = "Hello World";
+    // alert(message);
+
+    // Q10: Student's biodata
+    // var studentName = "Ali Khan";
+    // var studentAge = "20 years old";
+    // var studentCourse = "Web and Mobile App Development";
+    // alert(studentName);
+    // alert(studentAge);
+    // alert(studentCourse);
+
+    // Q11: Email message
+    // var email = "example@example.com";
+    // alert("My email address is " + email);
+
+    // Q12: Display message in browser
+    // document.write("Yay! I can write HTML content through JavaScript<br>");
+
+    // Q13: Store and display special string
+    // var special = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+    // alert(special);
+    // document.write(special + "<br>");
+
+    // Q14: Age variable
+    // var age = 22;
+    // alert("I am " + age + " years old");
+
+    // Q15: Visitor counter
+    // var visitCount = 5;
+    // alert("You have visited this site " + visitCount + " times");
+
+    // Q16: Birth year
+    // var birthYear = 2002;
+    // document.write("My birth year is " + birthYear + "<br>Data type of my declared variable is " + typeof birthYear + "<br>");
+
+    // Q17: Store visitor data
+    // var visitorName = "John Doe";
+    // var product = "T-shirt(s)";
+    // var quantity = 5;
+    // document.write(visitorName + " ordered " + quantity + " " + product + " on XYZ Clothing store<br>");
+
+    // Q18: Declare 3 variables in one statement
+    // var a, b, c;
+
+    // Q19: Legal and Illegal variable names
+    // Legal:
+    // var $name, _name, user1, fullName, camelCase;
+    // Illegal (do not uncomment, or they will cause errors):
+    // var 1name, full-name, alert, var, my name;
+
+    // Q20: Display rules
+    // document.write("<h2>Rules for naming JS variables</h2>");
+    // document.write("Variable names can only contain letters, numbers, $, and _. For example: $my_1stVariable<br>");
+    // document.write("Variables must begin with a letter, $, or _. For example: $name, _name or name<br>");
+    // document.write("Variable names are case sensitive<br>");
+    // document.write("Variable names should not be JS keywords<br>");
+
+    // Q21: Add two numbers
+    // var num1 = 10;
+    // var num2 = 20;
+    // var sum = num1 + num2;
+    // document.write("Sum of " + num1 + " and " + num2 + " is " + sum + "<br>");
+
+    // Q22: Arithmetic operations
+    // document.write("Subtraction: " + (num2 - num1) + "<br>");
+    // document.write("Multiplication: " + (num1 * num2) + "<br>");
+    // document.write("Division: " + (num2 / num1) + "<br>");
+    // document.write("Modulus: " + (num2 % num1) + "<br>");
+
+    // Q23: Math expressions
+    // var number;
+    // document.write("Value after variable declaration is: " + number + "<br>");
+    // number = 5;
+    // document.write("Initial value: " + number + "<br>");
+    // number++;
+    // document.write("Value after increment is: " + number + "<br>");
+    // number += 7;
+    // document.write("Value after addition is: " + number + "<br>");
+    // number--;
+    // document.write("Value after decrement is: " + number + "<br>");
+    // var remainder = number % 3;
+    // document.write("The remainder is: " + remainder + "<br>");
+
+    // Q24: Movie ticket cost
+  //     var ticketPrice = 600;
+  // var totalTickets = 5;
+  // var totalCost = ticketPrice * totalTickets;
+  // document.write("Total cost to buy " + totalTickets + " tickets is: " + totalCost + " PKR<br><br>");
+   // Q25: Multiplication table of a number
+  var number = 7;
+  for (var i = 1; i <= 10; i++) {
+    document.write(number + " x " + i + " = " + (number * i) + "<br>");
+  }
---
+++ JAVASCRIPT Assignment/JS Assignment 1/scriptQ5.js
@@ -0,0 +1,4 @@
+function showAlert() {
+    alert("Hello! This is an alert message from your project.");
+}
+
